9c3d1efe6258e5d3b0a45e38a589e72dcd96d8c3
yy <812169562@qq.com>
2019-10-09 16:36:13 +0800

    修改版本

--- src/Sand/GlobalSuppressions.cs
+++ src/Sand/GlobalSuppressions.cs
@@ -4,5 +4,6 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("样式", "IDE0041:使用 \"is null\" 检查", Justification = "<挂起>", Scope = "member", Target = "~M:Sand.Domain.Entities.Entity`1.GetHashCode~System.Int32")]
 
